45d33c8e47c9aa22932ddcb589d3a4649c2d3da4
SaiSmita9 <saismita.rath@gmail.com>
2018-09-06 17:08:50 +0530

    few lines added to the file

--- src/test/java/testsuites/Sortable.java
+++ src/test/java/testsuites/Sortable.java
@@ -1,5 +1,15 @@
 package testsuites;
 
+import org.testng.annotations.Test;
+
+import mainclasses.Homepage;
+
 public class Sortable {
 
+	@Test(priority=0)
+	 public void set() throws Exception {
+		
+		
+		 System.out.println("In Test priority 0");
+	 }
 }
