b6af4202d01ae1fe22d82c9d33abe1ca828e3317
SaiSmita9 <saismita.rath@gmail.com>
2018-09-06 13:09:31 +0530

    adding

--- src/test/java/testsuites/DraggableTest.java
+++ src/test/java/testsuites/DraggableTest.java
@@ -1,5 +1,12 @@
 package testsuites;
 
+import org.testng.annotations.Test;
+
 public class DraggableTest {
 
+	@Test(priority=0)
+	 public void drag() throws Exception {
+		
+		 System.out.println("drag it ");
+	 }
 }
